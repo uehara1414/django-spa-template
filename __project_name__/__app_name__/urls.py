e587f18c0cb14c7dd6b7ffaa97964f6207fec4ba
uehara <b1015062@fun.ac.jp>
2017-12-13 21:39:25 +0900

    remove index

--- __project_name__/__app_name__/urls.py
+++ __project_name__/__app_name__/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.staticfiles.views import serve
 from django.conf import settings
-from django.http import JsonResponse
 from .views import urlpatterns
-
-
-def index(request):
-    return JsonResponse({"Hello": "World"})
 
 
 urlpatterns = [
