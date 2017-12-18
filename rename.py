cf0425c3c02f2e84cf548fc94fbed2df462b6b6f
uehara <b1015062@fun.ac.jp>
2017-12-18 15:55:19 +0900

    add rename script

--- rename.py
+++ rename.py
@@ -1,23 +1,38 @@
 import os
 
-rename_patterns = [
+RENAME_PATTERNS = [
     ('__project_name__', '{{cookiecutter.project_name}}'),
     ('__app_name__', '{{cookiecutter.app_name}}'),
 ]
 
+IGNORE_FILES = ['rename.py', 'README.md']
+IGNORE_DIRS = ['.git']
+
 
 def replace(text):
     output = text
-    for pattern in rename_patterns:
+    for pattern in RENAME_PATTERNS:
         output = output.replace(pattern[0], pattern[1])
     return output, text != output
 
 
+def is_ignore_dir(dirpath):
+    for dir in IGNORE_DIRS:
+        if dir in dirpath:
+            return True
+    return False
+
+
 def main():
     for dirpath, dirnames, filenames in os.walk('.'):
+        if is_ignore_dir(dirpath):
+            continue
+
         for filename in filenames:
             fullpath = os.path.join(dirpath, filename)
-            with open(fullpath) as fp:
+            if filename in IGNORE_FILES:
+                continue
+            with open(fullpath, 'r') as fp:
                 text = fp.read()
             text, changed = replace(text)
             if changed:
@@ -31,6 +46,8 @@ def main():
                 os.rename(fullpath, new_fullpath)
 
         for filename in filenames:
+            if filename in IGNORE_FILES:
+                continue
             fullpath = os.path.join(dirpath, filename)
             new_fullpath, changed = replace(fullpath)
             if changed:
